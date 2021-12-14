a053014e90f98dda1d3e4863c52ff10b9033a791
Charlotte Curtis <c.f.curtis@gmail.com>
2021-12-13 21:53:48 -0700

    Done day 13, stupid off-by-one error

--- day13/day13.py
+++ day13/day13.py
@@ -19,41 +19,47 @@ def read_input(filename):
     
     return dots, folds
 
+def get_dot_range(dots):
+    min_x = min([dot[0] for dot in dots])
+    max_x = max([dot[0] for dot in dots])
+    min_y = min([dot[1] for dot in dots])
+    max_y = max([dot[1] for dot in dots])
+    return min_x, max_x, min_y, max_y
+
 def fold(dots,fold):
     if fold[0] == 0:
         # y axis fold
-        return set([(dot[0],abs(dot[1] - fold[1]) + fold[1]) for dot in dots])
+        return set([(dot[0], abs(dot[1] - fold[1]) - 1) for dot in dots])
     else:
         # x axis fold
-        return set([(abs(dot[0] - fold[0]) + fold[0], dot[1]) for dot in dots])
+        return set([(abs(dot[0] - fold[0]) - 1, dot[1]) for dot in dots])
 
 def display(dots):
-    min_x = min([dot[0] for dot in dots])
-    min_y = min([dot[1] for dot in dots])
-    max_x = max([dot[0] for dot in dots])
-    max_y = max([dot[1] for dot in dots])
-
     grid = []
-    for row in range(max_y - min_y + 1):
-        grid.append(['.'] * (max_x - min_x + 1))
+    _, max_x, _, max_y = get_dot_range(dots)
+
+    for row in range(max_y + 1):
+        grid.append(['.'] * (max_x + 1))
 
     for dot in dots:
-        grid[dot[1] - min_y][dot[0] - min_x] = '#'
+        grid[dot[1]][dot[0]] = '#'
     
-    with open('output.txt','w') as f:
-        for row in grid:
-            f.write("".join(row) + '\n')
+    for row in grid[::-1]:
+        print("".join(row[::-1]))
 
 if __name__ == '__main__':
     dots, folds = read_input('input.txt')
+
+    print('Initial range:')
+    print(get_dot_range(dots))
     
     # part 1
     folded = fold(dots,folds[0])
     print(len(folded))
 
     # part 2
-    for f in folds[1:]:
-        folded = fold(folded,f)
-    
+    for f in folds:
+        dots = fold(dots,f)
+
     # visualize
-    display(folded)
+    display(dots)
